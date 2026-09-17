ca0470e34ba4fad154ab240bfd22ab417d83d21a
Test02-Thuylt <thuthuy7692@gmail.com>
2026-09-17 16:36:33 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/calm-belt-2.js
@@ -0,0 +1,7 @@
+let deviFruit = "gomu-gomu";
+
+if (deviFruit === "gomu-gomu") { console.log("Gear 5 unlock!") }
+else if (deviFruit === "mera-mera") { console.log("Flame Emperor!") }
+else if (deviFruit === "ope-ope") { console.log("Room & Ope!") }
+else if (deviFruit === "none") { console.log("Normal Human!") }
+else { console.log("Unknow Devil Fruit") };
